0b800a73a66b0345688ae9216db0cc0f8801380a
haeseonkim <khs51777@naver.com>
2019-09-04 22:52:22 +0900

    더하기
    숫자와 콤마로만 이루어진 문자열 S가 주어진다. 이때, S에 포함되어있는 자연수의 합을 구하는 프로그램을 작성하시오.
    
    S의 첫 문자와 마지막 문자는 항상 숫자이고, 콤마는 연속해서 주어지지 않는다. 주어지는 수는 항상 자연수이다.

---
+++ 10822번.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string s;
+	string t;
+	int sum = 0, num = 0;
+	cin >> s;
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == ',') {
+			sum += stoi(t); 
+			t = "";		
+		}
+		else {
+			t += s[i];
+		}
+	}
+	cout << sum + stoi(t) << endl;;
+	return 0;
+}
